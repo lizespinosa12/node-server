c7cc56d0145093a1a08dbccf87119caca0dcb223
liz <lizethe1292@gmail.com>
2023-10-01 18:01:16 -0500

    he cambiado las funciones de agregar, borrar y completar tarea a promesas y usé await dentro de promptForAction

--- app.js
+++ app.js
@@ -16,49 +16,60 @@ function displayTasks() {
 }
 
 function addTask(description) {
-  tasks.push({ description, completed: false });
-  console.log(`Tarea "${description}" añadida.`);
-  displayTasks();
+  return new Promise((resolve, reject) => {
+    tasks.push({ description, completed: false });
+    console.log(`Tarea "${description}" añadida.`);
+    displayTasks();
+    resolve();
+  });
 }
 
 function deleteTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    const deletedTask = tasks.splice(index, 1);
-    console.log(`Tarea "${deletedTask[0].description}" eliminada.`);
-  } else {
-    console.log('Índice de tarea no válido.');
-  }
-  displayTasks();
+  return new Promise((resolve, reject) => {
+    if (index >= 0 && index < tasks.length) {
+      const deletedTask = tasks.splice(index, 1);
+      console.log(`Tarea "${deletedTask[0].description}" eliminada.`);
+      displayTasks();
+      resolve();
+    } else {
+      console.log('Índice de tarea no válido.');
+      reject();
+    }
+  });
 }
 
 function completeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks[index].completed = true;
-    console.log(`Tarea "${tasks[index].description}" marcada como completada.`);
-  } else {
-    console.log('Índice de tarea no válido.');
-  }
-  displayTasks();
+  return new Promise((resolve, reject) => {
+    if (index >= 0 && index < tasks.length) {
+      tasks[index].completed = true;
+      console.log(`Tarea "${tasks[index].description}" marcada como completada.`);
+      displayTasks();
+      resolve();
+    } else {
+      console.log('Índice de tarea no válido.');
+      reject();
+    }
+  });
 }
 
 function promptForAction() {
-  rl.question('Elije una acción (1 para añadir tarea, 2 para eliminar tarea, 3 para completar tarea, 0 para salir): ', (choice) => {
+  rl.question('Elije una acción (1 para añadir tarea, 2 para eliminar tarea, 3 para completar tarea, 0 para salir): ', async (choice) => {
     if (choice === '1') {
-      rl.question('Ingrese la descripción de la tarea: ', (description) => {
-        addTask(description);
+      rl.question('Ingrese la descripción de la tarea: ', async (description) => {
+        await addTask(description);
         promptForAction();
       });
     } else if (choice === '2') {
-      rl.question('Ingrese el número de la tarea que desea eliminar: ', (index) => {
-        deleteTask(parseInt(index) - 1);
+      rl.question('Ingrese el número de la tarea que desea eliminar: ', async (index) => {
+        await deleteTask(parseInt(index) - 1);
         promptForAction();
       });
     } else if (choice === '3') {
-      rl.question('Ingrese el número de la tarea que desea marcar como completada: ', (index) => {
-        completeTask(parseInt(index) - 1);
+      rl.question('Ingrese el número de la tarea que desea marcar como completada: ', async (index) => {
+        await completeTask(parseInt(index) - 1);
         promptForAction();
       });
-    } else if (choice === '0') {
+       } else if (choice === '0') {
       rl.close();
     } else {
       console.log('Acción no válida.');
@@ -68,6 +79,5 @@ function promptForAction() {
 }
 
 console.log('Bienvenido a la Lista de Tareas.');
-
 
 promptForAction();
